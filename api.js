33cf2e659646278263bca72d1614d08b4393606b
Oliver Crawford <oicrawford@gmail.com>
2024-04-21 10:07:32 -0500

    made the submission page

--- api.js
+++ api.js
@@ -1,2 +1,24 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/files/index.html', );
+});
+
+app.post('/submit', function (req, res) {
+    res.send('POST Request');
+});
+
+app.put('/update-data', function (req, res) {
+    res.send('PUT Request');
+});
+
+app.delete('/delete-data', function (req, res) {
+    res.send('DELETE Request');
+});
+
+var server = app.listen(8080, function () {
+    console.log('Node server is running..');
+});
